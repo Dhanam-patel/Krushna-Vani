8272863d3061e8ec6a3fa040c90f1be51918857f
Dhanam-patel <dhanampatel2005@gmail.com>
2025-11-24 01:48:43 +0530

    Commit messages

--- Vector_Search/Search.py
+++ Vector_Search/Search.py
@@ -1,4 +1,3 @@
-from pinecone import Pinecone
 from Embedding_Pipeline.Pinecone_setup import index
 
 def Vector_searching(vectors: dict):
@@ -6,7 +5,7 @@ def Vector_searching(vectors: dict):
         namespace="ns1",
         vector= vectors["values"],
         include_metadata=True,
-        top_k=1,
+        top_k=3,
     )
     
     return Results
